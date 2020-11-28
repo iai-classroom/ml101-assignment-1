18f795d48b9bd110e6de59d209711aa2a147de47
Ravi Mashru <mashru.ravi@gmail.com>
2020-11-28 14:58:23 +0530

    Create replit.py

---
+++ replit.py
@@ -0,0 +1,16 @@
+# Question 1
+print('Question 1')
+print('----------')
+print('Expected:')
+print('Hello, World!')
+print('Actual:')
+import q1
+
+# Question 2
+print('Question 1')
+print('----------')
+print('Expected:')
+print('Hello, World!')
+print('HELLO, WORLD!')
+print('Actual:')
+import q2
